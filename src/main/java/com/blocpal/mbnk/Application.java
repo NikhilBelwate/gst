6efc2f6ee0b974eb947b6edd1c2bcb3a898bb1f8
NIKHIL BELWATE <nikhil.beal@gmail.com>
2022-06-16 20:59:57 +0000

    Initial commit

---
+++ src/main/java/com/blocpal/mbnk/Application.java
@@ -0,0 +1,9 @@
+package com.blocpal.mbnk;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
